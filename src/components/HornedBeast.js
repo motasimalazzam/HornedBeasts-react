36929823072fcf954f57cde4bc9a80b072ed8199
motasimalazzam <motasim.h.alazzam@gmail.com>
2021-05-21 21:15:41 +0300

    finish lab4

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -38,7 +38,7 @@ class HornedBeast extends React.Component {
                 
 
                 <Card onClick={this.popUpTheCard} style={{ width: '18rem'}}>
-                    <Card.Img variant="top" src={this.props.image_url} alt={this.props.title} title={this.props.title}  />
+                    <Card.Img variant="top" src={this.props.image_url} alt={this.props.title} title={this.props.title} style={{ height: '18rem'}} />
                     <Card.Body>
                         <Card.Title>{this.props.title}</Card.Title>
                         <Card.Text>
